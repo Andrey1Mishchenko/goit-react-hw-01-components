5c61d7ed4f20c4beb86e7a7c1729b89c6891de11
Andrey1Mishchenko <andrej1mishchenko@gmail.com>
2022-01-23 22:19:44 +0200

    создано все компоненты + стили для Profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,27 @@
-export default function App() {
-  return <div></div>;
-}
+import Profile from "../components/profile/Profile";
+import profileData from "./user.json";
+import Statistics from "../components/statistics/Statistics";
+import statisticsData from "./data.json";
+import FriendList from "./friends/FriendList";
+import friendsData from "./friends.json";
+import TransactionHistory from "./transaction/TransactionHistory";
+import transactionData from "./transactions.json";
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        username={profileData.username}
+        tag={profileData.tag}
+        location={profileData.location}
+        avatar={profileData.avatar}
+        stats={profileData.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticsData} />
+      <FriendList friends={friendsData} />
+      <TransactionHistory items={transactionData} />
+    </div>
+  );
+};
+
+export default App;
